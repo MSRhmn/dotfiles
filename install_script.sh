25a7975ab8157cacaa55a567fb5a1c17f375f432
Md. Shamim Rahman <apubdtea@gmail.com>
2023-12-02 16:38:03 +0600

    install script updated

--- install_script.sh
+++ install_script.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-# Tools
-sudo apt install tree dos2unix -y 
+
+# Software & Tools
+
+sudo apt update -y
+sudo apt install git curl tree dos2unix gnome-tweaks gnome-clocks fonts-firacode ibus-avro mpv keepassxc qbittorrent -y 
+
+
+# Removing pre-installed softwares
+
+sudo snap remove firefox -y
+sudo apt remove thunderbird -y
+sudo apt autoremove -y
+
