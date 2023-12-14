12cb22a15ad2c339cd0dc2956477c11ea5aa3c26
Md. Shamim Rahman <apubdtea@gmail.com>
2023-12-14 13:34:52 +0600

    install script updated

--- install_script.sh
+++ install_script.sh
@@ -4,12 +4,12 @@
 # Installing Softwares & Tools
 
 sudo apt update -y
-sudo apt install git curl black python3-pip tree dos2unix gnome-tweaks gnome-clocks fonts-noto-core fonts-firacode ibus-avro mpv keepassxc qbittorrent -y 
+sudo apt install git curl vim black python3-pip tree dos2unix gnome-tweaks gnome-clocks fonts-noto-core fonts-firacode ibus-avro mpv keepassxc qbittorrent -y 
 
 
 # Removing Pre-installed Softwares
 
 sudo snap remove firefox -y
-sudo apt remove thunderbird -y
+sudo apt remove thunderbird remmina -y
 sudo apt autoremove -y
 
