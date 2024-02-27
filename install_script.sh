34b9638dd80a101845b0994aeb451fa17e946b70
Md. Shamim Rahman <apubdtea@gmail.com>
2024-02-27 21:29:37 +0600

    chore: add python3-venv in the install

--- install_script.sh
+++ install_script.sh
@@ -4,7 +4,7 @@
 # Install Softwares & Tools
 
 sudo apt update -y
-sudo apt install git curl vim black python3-pip tree dos2unix gnome-tweaks gnome-clocks fonts-noto-core fonts-firacode ibus-avro mpv keepassxc qbittorrent -y 
+sudo apt install git curl vim black python3-pip python3-venv tree dos2unix gnome-tweaks gnome-clocks fonts-noto-core fonts-firacode ibus-avro mpv keepassxc qbittorrent -y 
 
 # Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
