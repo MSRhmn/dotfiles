b46066982ec8270df9d48dd866de6652e0bd10eb
Md. Shamim Rahman <apubdtea@gmail.com>
2024-04-28 18:19:24 +0600

    feat: add ubuntu desktop specific scripts

--- ubuntu_scripts.sh
+++ ubuntu_scripts.sh
@@ -15,10 +15,7 @@ Exec=/usr/bin/microsoft-edge-stable %U--enable-features=ScrollableTabStrip
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 
 # Bnalga font instructions
-  - For Bangla fonts conflict remove all the bengali fonts from the "/usr/share/fonts/truetype" directory also remove the "/freefont" directory.
-  - Copy the "Noto Sans Bengali" into "/usr/share/fonts/truetype" and reboot.
   - For Bangla fonts list "apt-cache search bengali"
-  - To install sudo apt-get install fonts-beng fonts-beng-extra fonts-lohit-beng-bengali; (i.e.)
 	
 # Chrome youtube video horizontal stripe fix in intel based graphics
 sudo apt install '*video-intel*'
