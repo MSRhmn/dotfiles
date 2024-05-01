66929e3cae45d5e7d08ed4e9c16f22184292bbec
Md. Shamim Rahman <apubdtea@gmail.com>
2024-05-01 11:41:45 +0600

    feat: add deb version firefox install script

--- install_script.sh
+++ install_script.sh
@@ -22,6 +22,17 @@ sudo apt install \
   libreoffice \
   qbittorrent -y
 
+# Install deb version of Firefox
+sudo install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla
+sudo apt-get update && sudo apt-get install firefox
+
 # Install nvm (Node Version Manager)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
