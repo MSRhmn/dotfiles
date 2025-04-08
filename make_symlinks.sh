3b9b916e039469cc9bad4d7a04ae303484c90f9a
Md. Shamim Rahman <apubdtea@gmail.com>
2025-04-08 16:46:55 +0600

    feat: users bin directory

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -3,20 +3,23 @@
 # Define dotfiles directory path
 DOTFILES_DIR="${HOME}/dotfiles"
 
+# Create ~/bin directory if it doesn't exist
+mkdir -p "${HOME}/bin"
+
 # Symlink scripts to user's bin directory
 for script in "install_script.sh" "update_script.sh"; do
-  ln -sf "${DOTFILES_DIR}/${script}" ~/bin/"${script}"
+  ln -sf "${DOTFILES_DIR}/${script}" "${HOME}/bin/${script}"
 done
 
 # Symlink configuration files to user's home directory
 for file in ".bashrc" ".bash_aliases" ".profile" ".gitconfig"; do
-  ln -sf "${DOTFILES_DIR}/${file}"  ~/"${file}"
+  ln -sf "${DOTFILES_DIR}/${file}" "${HOME}/${file}"
 done
 
 # Handle potential errors
 if [[ $? -ne 0 ]]; then
-  echo "Error occurred during symlinking process."
+  echo "❌ Error occurred during symlinking process."
   exit 1
 fi
 
-echo "Symlinks created successfully!"
+echo "✅ Symlinks created successfully!"
