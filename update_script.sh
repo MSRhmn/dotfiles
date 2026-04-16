5eebefb00d932f9274c24712ec49d9e32cf4b3f9
Md. Shamim Rahman <apubdtea@gmail.com>
2026-04-16 17:23:38 +0600

    remove: discord update script

--- update_script.sh
+++ update_script.sh
@@ -28,27 +28,3 @@ if [ -d "$HOME/.nvm" ]; then
 fi
 
 echo "=== System Update Completed Successfully ==="
-
-# # === Discord update script ===
-# echo ==== Checking For Updates ====; echo;
-
-# # checking for discord app
-# if ! command -v discord &> /dev/null
-# then
-#    echo Discord could not be found!
-#    exit 1
-# fi
-
-# if command -v discord >/dev/null 2>&1; then
-#   echo "Updating Discord..."
-
-#   TMP_DEB="/tmp/discord.deb"
-
-#   wget -O "$TMP_DEB" "https://discord.com/api/download?platform=linux&format=deb"
-
-#   sudo apt install -y "$TMP_DEB"
-
-#   rm -f "$TMP_DEB"
-# else
-#   echo "Discord not installed. Skipping."
-# fi
