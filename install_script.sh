f44fc915abdbb62d0386cc949b4e5ac19a049aa9
Md. Shamim Rahman <apubdtea@gmail.com>
2026-05-12 18:16:57 +0600

    update: brave script

--- install_script.sh
+++ install_script.sh
@@ -73,12 +73,11 @@ fi
 if ! command -v brave-browser >/dev/null 2>&1; then
   echo "Installing Brave..."
 
-  sudo mkdir -p /usr/share/keyrings
+  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg \
+    https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
-  curl -fsSL https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg \
-    -o /usr/share/keyrings/brave-browser-archive-keyring.gpg
-
-  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" |
+  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] \
+https://brave-browser-apt-release.s3.brave.com/ stable main" |
     sudo tee /etc/apt/sources.list.d/brave-browser-release.list >/dev/null
 
   sudo apt update
