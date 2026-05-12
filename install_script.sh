3d764c5db028403be9126eb112cc45c2b48c1b21
Md. Shamim Rahman <apubdtea@gmail.com>
2026-05-12 18:51:58 +0600

    update: chrome script

--- install_script.sh
+++ install_script.sh
@@ -140,24 +140,29 @@ fi
 
 # === Install google chrome ===
 if ! command -v google-chrome >/dev/null 2>&1; then
-  read -p "Install Google Chrome? (y/n): " answer
+
+  read -rp "Install Google Chrome? (y/n): " answer
+
   if [[ "$answer" =~ ^[Yy]$ ]]; then
-    echo "Installing Chrome..."
+
+    echo "=== Installing Google Chrome ==="
+
+    sudo mkdir -p /etc/apt/keyrings
 
     curl -fsSL https://dl.google.com/linux/linux_signing_key.pub |
-      sudo gpg --dearmor -o /usr/share/keyrings/google-chrome.gpg
+      gpg --dearmor |
+      sudo tee /etc/apt/keyrings/google-chrome.gpg >/dev/null
 
-    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main" |
+    echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/google-chrome.gpg] https://dl.google.com/linux/chrome/deb/ stable main" |
       sudo tee /etc/apt/sources.list.d/google-chrome.list >/dev/null
 
     sudo apt update
     sudo apt install -y google-chrome-stable
-
   else
     echo "Chrome skipped."
   fi
 else
-  echo "Chrome already installed."
+  echo "Google Chrome already installed."
 fi
 
 # === Install nvm (Node Version Manager) ===
