e83e7f49f624ed9b6529a834a3a7f4db1e1918e1
Md. Shamim Rahman <apubdtea@gmail.com>
2026-05-16 17:56:48 +0600

    update: install script

--- install_script.sh
+++ install_script.sh
@@ -150,33 +150,6 @@ else
   echo "Microsoft Edge is already installed."
 fi
 
-# === Install google chrome ===
-if ! command -v google-chrome >/dev/null 2>&1; then
-
-  read -rp "Install Google Chrome? (y/n): " answer
-
-  if [[ "$answer" =~ ^[Yy]$ ]]; then
-
-    echo "=== Installing Google Chrome ==="
-
-    sudo mkdir -p /etc/apt/keyrings
-
-    curl -fsSL https://dl.google.com/linux/linux_signing_key.pub |
-      gpg --dearmor |
-      sudo tee /etc/apt/keyrings/google-chrome.gpg >/dev/null
-
-    echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/google-chrome.gpg] https://dl.google.com/linux/chrome/deb/ stable main" |
-      sudo tee /etc/apt/sources.list.d/google-chrome.list >/dev/null
-
-    sudo apt update
-    sudo apt install -y google-chrome-stable
-  else
-    echo "Chrome skipped."
-  fi
-else
-  echo "Google Chrome already installed."
-fi
-
 # === Install nvm (Node Version Manager) ===
 if [ ! -d "$HOME/.nvm" ] && [ ! -d "$HOME/.config/nvm"]; then
   echo "Installing nvm..."
